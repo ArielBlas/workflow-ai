d0daae750492df7ba8cdaf3cbc7b5f82e8604942
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-03-14 23:54:51 -0300

    feat: DesktopSidebar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import layout from "../layout";
 import { Separator } from "react-resizable-panels";
+import DesktopSidebar from "@/components/Sidebar";
 
 type Props = {
   children: React.ReactNode;
@@ -9,13 +10,14 @@ type Props = {
 const Layout = ({ children }: Props) => {
   return (
     <div className="flex h-screen">
+      <DesktopSidebar />
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           Workflow AI
         </header>
         <Separator />
         <div className="overflow-auto">
-          <div className="flex-1 contianer py-4 text-accent-foreground">
+          <div className="flex-1 container py-4 text-accent-foreground">
             {children}
           </div>
         </div>
