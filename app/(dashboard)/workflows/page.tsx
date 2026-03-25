c88c7b32b2aae26edbcbb3ab2ba2caef8ca546de
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-03-24 23:57:05 -0300

    feat: GetWorkflowsForUser action

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -1,6 +1,8 @@
+import { GetWorkflowsForUser } from "@/actions/workflows/getWorkflowsForUser";
 import { Skeleton } from "@/components/ui/skeleton";
-import { waitFor } from "@/lib/helper/waitFor";
 import React, { Suspense } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle, InboxIcon } from "lucide-react";
 
 type Props = {};
 
@@ -34,8 +36,35 @@ function UserWorkflowsSkeleton() {
 }
 
 async function UserWorkflows() {
-  await waitFor(3000);
-  return <div></div>;
+  try {
+    const workflows = await GetWorkflowsForUser();
+
+    if (workflows.length === 0) {
+      <div className="flex flex-col gap-4 h-full items-center justify-center">
+        <div className="rounded-full bg-accent w-20 h-20 flex items-center justify-center">
+          <InboxIcon size={40} className="stroke-primary" />
+        </div>
+        <div className="flex flex-col gap-1 text-center">
+          <p className="font-bold">No workflows created yet</p>
+          <p className="text-sm text-muted-foreground">
+            Click the button below to create your first workflow
+          </p>
+        </div>
+      </div>;
+    }
+
+    return <div></div>;
+  } catch (error) {
+    return (
+      <Alert variant="destructive">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          Something went wrong. Please try again later
+        </AlertDescription>
+      </Alert>
+    );
+  }
 }
 
 export default page;
