f7fa56dab1986bbe0777a15d40464be5ab5ea28a
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-03-31 23:57:37 -0300

    refactor: WorkflowCard component

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { WorkflowStatus } from "@/types/workflow";
 import { Workflow } from "@prisma/client";
-import { FileTextIcon, PlayIcon } from "lucide-react";
+import { FileTextIcon, PlayIcon, ShuffleIcon } from "lucide-react";
 import Link from "next/link";
 
 const statusColors = {
@@ -21,27 +22,52 @@ const WorkflowCard = ({ workflow }: Props) => {
   return (
     <Card className="border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark:shadow-primary/30">
       <CardContent className="p-4 flex items-center justify-between h-[100px] ">
-        <div
-          className={cn(
-            "w-10 h-10 rounded-full flex items-center justify-center",
-            statusColors[workflow.status as WorkflowStatus],
-          )}
-        >
-          {isDraft ? (
-            <FileTextIcon className="h-5 w-5" />
-          ) : (
-            <PlayIcon className="h-5 w-5 text-white" />
-          )}
+        <div className="flex items-center justify-end space-x-3">
+          <div
+            className={cn(
+              "w-10 h-10 rounded-full flex items-center justify-center",
+              statusColors[workflow.status as WorkflowStatus],
+            )}
+          >
+            {isDraft ? (
+              <FileTextIcon className="h-5 w-5" />
+            ) : (
+              <PlayIcon className="h-5 w-5 text-white" />
+            )}
+          </div>
+          <div>
+            <h3 className="text-base font-bold text-muted-foreground flex items-center">
+              <Link
+                href={`/workflow/editor/${workflow.id}`}
+                className="flex items-center hover:underline"
+              >
+                {workflow.name}
+              </Link>
+              {isDraft && (
+                <span
+                  className="ml-2 px-2 py-0.6 text-xs font-medium
+               bg-yellow-100 text-yellow-800 rounded-full"
+                >
+                  Draft
+                </span>
+              )}
+            </h3>
+          </div>
         </div>
-        <div>
-          <h3 className="text-base font-bold text-muted-foreground flex items-center">
-            <Link
-              href={`/workflow/editor/${workflow.id}`}
-              className="flex items-center hover:underline"
-            >
-              {workflow.name}
-            </Link>
-          </h3>
+        <div className="flex items-center space-x-2">
+          <Link
+            href={`/workflow/editor/${workflow.id}`}
+            className={cn(
+              buttonVariants({
+                variant: "outline",
+                size: "sm",
+              }),
+              "flex items-center gap-2",
+            )}
+          >
+            <ShuffleIcon size={16} />
+            Edit
+          </Link>
         </div>
       </CardContent>
     </Card>
