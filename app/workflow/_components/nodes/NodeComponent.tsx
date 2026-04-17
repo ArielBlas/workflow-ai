53de20e8e9fdb75ae4bd4e1dbed9d58cbe9fa6cb
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-04-16 23:54:44 -0300

    feat: NodeInputs component

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -3,12 +3,22 @@ import { NodeProps } from "@xyflow/react";
 import NodeCard from "./NodeCard";
 import NodeHeader from "./NodeHeader";
 import { AppNodeData } from "@/types/appNode";
+import { NodeInput, NodeInputs } from "./NodeInputs";
+import { TaskRegistry } from "@/lib/workflow/task/registry";
 
 const NodeComponent = memo((props: NodeProps) => {
   const nodeData = props.data as AppNodeData;
+  const task = TaskRegistry[nodeData.type];
+
   return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected}>
       <NodeHeader taskType={nodeData.type} />
+
+      <NodeInputs>
+        {task.inputs.map((input) => (
+          <NodeInput key={input.name} input={input} />
+        ))}
+      </NodeInputs>
     </NodeCard>
   );
 });
