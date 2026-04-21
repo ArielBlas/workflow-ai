23fe0d473ba948cb6616e19d5fba2098afa19c7a
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-04-20 23:58:16 -0300

    feat: Topbar component

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Workflow } from "@prisma/client";
 import { ReactFlowProvider } from "@xyflow/react";
 import FlowEditor from "./FlowEditor";
+import Topbar from "./topbar/Topbar";
 
 type Props = {
   workflow: Workflow;
@@ -13,6 +14,7 @@ const Editor = ({ workflow }: Props) => {
   return (
     <ReactFlowProvider>
       <div className="flex flex-col h-full w-full overflow-hidden">
+        <Topbar />
         <section className="flex h-full overflow-auto">
           <FlowEditor workflow={workflow} />
         </section>
