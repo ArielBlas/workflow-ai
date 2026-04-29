1e1c30f65986df5f6c8ed7c9fd34101714ad4f47
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-04-28 23:47:38 -0300

    refactor: workflow task

--- lib/workflow/task/PageToHtml.tsx
+++ lib/workflow/task/PageToHtml.tsx
@@ -15,4 +15,14 @@ export const PageToHtmlTask = {
       hideHandle: true,
     },
   ],
+  outputs: [
+    {
+      name: "HTML",
+      type: TaskParamType.STRING,
+    },
+    {
+      name: "Web page",
+      type: TaskParamType.BROWSER_INSTANCE,
+    },
+  ],
 };
