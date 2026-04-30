503018c36dd4a947794149598deb28fc47c268b0
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-04-29 23:21:47 -0300

    feat: NodeOutputs component

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -5,6 +5,7 @@ import NodeHeader from "./NodeHeader";
 import { AppNodeData } from "@/types/appNode";
 import { NodeInput, NodeInputs } from "./NodeInputs";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
+import { NodeOutput, NodeOutputs } from "./NodeOutputs";
 
 const NodeComponent = memo((props: NodeProps) => {
   const nodeData = props.data as AppNodeData;
@@ -19,6 +20,12 @@ const NodeComponent = memo((props: NodeProps) => {
           <NodeInput key={input.name} input={input} nodeId={props.id} />
         ))}
       </NodeInputs>
+
+      <NodeOutputs>
+        {task.outputs.map((output) => (
+          <NodeOutput key={output.name} output={output} nodeId={props.id} />
+        ))}
+      </NodeOutputs>
     </NodeCard>
   );
 });
