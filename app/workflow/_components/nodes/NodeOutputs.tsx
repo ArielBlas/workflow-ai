ae6245a33b96c0540c8923d7f0e21fd128f14ea7
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-04-30 23:47:22 -0300

    feat: ColorForHandle common

--- app/workflow/_components/nodes/NodeOutputs.tsx
+++ app/workflow/_components/nodes/NodeOutputs.tsx
@@ -4,18 +4,13 @@ import { cn } from "@/lib/utils";
 import { TaskParam } from "@/types/task";
 import { Handle, Position } from "@xyflow/react";
 import { ReactNode } from "react";
+import { ColorForHandle } from "./common";
 
 export function NodeOutputs({ children }: { children: ReactNode }) {
   return <div className="flex flex-col divide-y gap-1">{children}</div>;
 }
 
-export function NodeOutput({
-  output,
-  nodeId,
-}: {
-  output: TaskParam;
-  nodeId: string;
-}) {
+export function NodeOutput({ output }: { output: TaskParam }) {
   return (
     <div className="flex justify-end relative p-3 bg-secondary">
       <p className="text-xs text-muted-foregroun">{output.name}</p>
@@ -25,6 +20,7 @@ export function NodeOutput({
         position={Position.Right}
         className={cn(
           "!bg-muted-foreground !border-2 !border-background !-right-2 !w-4 !h-4",
+          ColorForHandle[output.type],
         )}
       />
     </div>
