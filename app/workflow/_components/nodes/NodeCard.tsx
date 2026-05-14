ceabdbeb1a960638ea739af4af861706e2f3ab19
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-05-13 23:55:43 -0300

    feat: useExecutionPlan handleError

--- app/workflow/_components/nodes/NodeCard.tsx
+++ app/workflow/_components/nodes/NodeCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import useFlowValidation from "@/components/hooks/useFlowValidation";
 import { cn } from "@/lib/utils";
 import { useReactFlow } from "@xyflow/react";
 import React from "react";
@@ -11,6 +12,8 @@ type Props = {
 
 const NodeCard = ({ nodeId, children, isSelected }: Props) => {
   const { getNode, setCenter } = useReactFlow();
+  const { invalidInputs } = useFlowValidation();
+  const hasInvalidInputs = invalidInputs.some((node) => node.nodeId === nodeId);
 
   return (
     <div
@@ -33,6 +36,7 @@ const NodeCard = ({ nodeId, children, isSelected }: Props) => {
       className={cn(
         "rounded-md cursor-pointer bg-background border-2 border-separate w-[420px] text-xs gap-1 flex flex-col",
         isSelected && "border-primary",
+        hasInvalidInputs && "border-destructive border-2",
       )}
     >
       {children}
