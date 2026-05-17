4d80225a21a347aa66de31835f9178aa6983e8d1
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-05-16 23:22:04 -0300

    refactor: ExecuteBtn component

--- app/workflow/_components/topbar/ExecuteBtn.tsx
+++ app/workflow/_components/topbar/ExecuteBtn.tsx
@@ -2,6 +2,10 @@ import { Button } from "@/components/ui/button";
 import { PlayIcon } from "lucide-react";
 import React from "react";
 import useExecutionPlan from "@/components/hooks/useExecutionPlan";
+import { useMutation } from "@tanstack/react-query";
+import { RunWorkflow } from "@/actions/workflows/runWorkflow";
+import { toast } from "sonner";
+import { useReactFlow } from "@xyflow/react";
 
 type Props = {
   workflowId: string;
@@ -9,13 +13,30 @@ type Props = {
 
 function ExecuteBtn({ workflowId }: Props) {
   const generate = useExecutionPlan();
+  const { toObject } = useReactFlow();
+
+  const mutation = useMutation({
+    mutationFn: RunWorkflow,
+    onSuccess: () => {
+      toast.success("Execution started", { id: "flow-execution" });
+    },
+    onError: () => {
+      toast.error("Something went wrong", { id: "flow-execution" });
+    },
+  });
   return (
     <Button
       variant={"outline"}
       className="flex items-center gap-2"
+      disabled={mutation.isPending}
       onClick={() => {
         const plan = generate();
-        console.table(plan);
+        if (!plan) return;
+
+        mutation.mutate({
+          workflowId,
+          flowDefinition: JSON.stringify(toObject()),
+        });
       }}
     >
       <PlayIcon size={16} className="stroke-orange-400" />
