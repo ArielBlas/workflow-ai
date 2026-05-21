26e975d9d9fe0ba6f08e1bcfb1bb6b6ee57dbdc6
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-05-20 23:53:54 -0300

    feat: Workflow enums

--- actions/workflows/runWorkflow.ts
+++ actions/workflows/runWorkflow.ts
@@ -3,7 +3,12 @@
 import prisma from "@/lib/prisma";
 import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
-import { WorkflowExecutionPlan } from "@/types/workflow";
+import {
+  ExecutionPhaseStatus,
+  WorkflowExecutionPlan,
+  WorkflowExecutionStatus,
+  WorkflowExecutionTrigger,
+} from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
 
 export async function RunWorkflow(form: {
@@ -56,15 +61,15 @@ export async function RunWorkflow(form: {
     data: {
       workflowId,
       userId,
-      status: "PENDING",
+      status: WorkflowExecutionStatus.RUNNING,
       startedAt: new Date(),
-      trigger: "manual",
+      trigger: WorkflowExecutionTrigger.MANUAL,
       phases: {
         create: executionPlan.flatMap((phase) => {
           return phase.nodes.flatMap((node) => {
             return {
               userId,
-              status: "CREATED",
+              status: ExecutionPhaseStatus.CREATED,
               number: phase.phase,
               node: JSON.stringify(node),
               name: TaskRegistry[node.data.type].label,
