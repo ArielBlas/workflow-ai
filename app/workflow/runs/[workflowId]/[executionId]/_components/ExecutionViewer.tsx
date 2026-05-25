a776c77e9577620180b188dd77261cd1af182924
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-05-24 23:58:27 -0300

    feat: ExecutionLabel

--- app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { GetWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowExecutionWithPhases";
 import { useQuery } from "@tanstack/react-query";
 import { WorkflowExecutionStatus } from "@/types/workflow";
-import { CalendarIcon, CircleDashedIcon } from "lucide-react";
+import {
+  CalendarIcon,
+  CircleDashedIcon,
+  ClockIcon,
+  CoinsIcon,
+  LucideIcon,
+  WorkflowIcon,
+} from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
+import { Separator } from "@/components/ui/separator";
 
 type ExecutionData = Awaited<ReturnType<typeof GetWorkflowExecutionWithPhases>>;
 
@@ -24,36 +32,58 @@ const ExecutionViewer = ({ initialData }: Props) => {
     <div className="flex w-full h-full">
       <aside className="w-[440px] min-w-[440px] max-w-[440px] border-r-2 border-separate flex flex-grow flex-col overflow-hidden">
         <div className="py-4 px-2">
-          <div className="flex justify-between items-center py-2 px-4">
-            <div className="text-muted-foreground flex items-center gap-2">
-              <CircleDashedIcon
-                size={20}
-                className="stroke-muted-foreground/80"
-              />
-              <span>Status</span>
-            </div>
-            <div className="font-semibold capitalize flex gap-2 items-center">
-              {query.data?.status}
-            </div>
-          </div>
-
-          <div className="flex justify-between items-center py-2 px-4 text-sm">
-            <div className="text-muted-foreground flex items-center gap-2">
-              <CalendarIcon size={20} className="stroke-muted-foreground/80" />
-              <span>Started at</span>
-            </div>
-            <div className="font-semibold capitalize flex gap-2 items-center">
-              {query.data?.startedAt
-                ? formatDistanceToNow(new Date(query.data.startedAt), {
-                    addSuffix: true,
-                  })
-                : "-"}
-            </div>
-          </div>
+          <ExecutionLabel
+            icon={CircleDashedIcon}
+            label="Status"
+            value={query.data?.status}
+          />
+          <ExecutionLabel
+            icon={CalendarIcon}
+            label="Started at"
+            value={
+              <span className="lowercase">
+                {query.data?.startedAt
+                  ? formatDistanceToNow(new Date(query.data.startedAt), {
+                      addSuffix: true,
+                    })
+                  : "-"}
+              </span>
+            }
+          />
+          <ExecutionLabel icon={ClockIcon} label="Duration" value="TODO" />
+          <ExecutionLabel
+            icon={CoinsIcon}
+            label="Credits consumed"
+            value="TODO"
+          />
         </div>
+        <Separator />
       </aside>
     </div>
   );
 };
+
+function ExecutionLabel({
+  icon,
+  label,
+  value,
+}: {
+  icon: LucideIcon;
+  label: ReactNode;
+  value: ReactNode;
+}) {
+  const Icon = icon;
+  return (
+    <div className="flex justify-between items-center py-2 px-4">
+      <div className="text-muted-foreground flex items-center gap-2">
+        <Icon size={20} className="stroke-muted-foreground/80" />
+        <span>{label}</span>
+      </div>
+      <div className="font-semibold capitalize flex gap-2 items-center">
+        {value}
+      </div>
+    </div>
+  );
+}
 
 export default ExecutionViewer;
