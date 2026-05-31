47b43718e507a1feed9b33d5746c08f65185fdab
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-05-30 23:44:17 -0300

    feat: initializeWorkflowExecution

--- lib/workflow/executeWorkflow.ts
+++ lib/workflow/executeWorkflow.ts
@@ -1,6 +1,7 @@
 import "server-only";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { WorkflowExecutionStatus } from "@/types/workflow";
 
 export async function ExecuteWorkflow(executionId: string) {
   const execution = await prisma.workflowExecution.findUnique({
@@ -20,4 +21,19 @@ export async function ExecuteWorkflow(executionId: string) {
   const environment = { phases: {} };
 
   revalidatePath("/workflows/runs");
+}
+
+async function initializeWorkflowExecution(
+  executionId: string,
+  workflowId: string,
+) {
+  await prisma.workflowExecution.update({
+    where: {
+      id: executionId,
+    },
+    data: {
+      startedAt: new Date(),
+      status: WorkflowExecutionStatus.RUNNING,
+    },
+  });
 }
