ac769192099f70bee933688cb5c5d75591449bc1
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-06-27 22:59:20 -0300

    feat: ReactCountUpWrapper component

--- components/UserAvailableCreditsBadge.tsx
+++ components/UserAvailableCreditsBadge.tsx
@@ -4,6 +4,8 @@ import { useQuery } from "@tanstack/react-query";
 import { CoinsIcon, Loader2Icon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import ReactCountUpWrapper from "./ReactCountUpWrapper";
+import { buttonVariants } from "@/components/ui/button";
 
 type Props = {};
 
@@ -14,11 +16,19 @@ const UserAvailableCreditsBadge = (props: Props) => {
     refetchInterval: 30 * 1000, // 30 seconds
   });
   return (
-    <Link href={"/billing"} className={cn("w-full space-x-2 items-center")}>
+    <Link
+      href={"/billing"}
+      className={cn(
+        "w-full space-x-2 items-center",
+        buttonVariants({ variant: "outline" }),
+      )}
+    >
       <CoinsIcon size={20} className="text-primary">
         <span className="font-semibold capitalize">
           {query.isLoading && <Loader2Icon className="w-4 h-4 animate-spin" />}
-          {!query.isLoading && query.data && query.data}
+          {!query.isLoading && query.data && (
+            <ReactCountUpWrapper value={query.data} />
+          )}
           {!query.isLoading && !query.data && "-"}
         </span>
       </CoinsIcon>
