ca4b3ed3f7689d59579f9cb8d7d2ca1afb680d77
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-07-02 23:57:37 -0300

    feat: GetWorkflowExecutions action

--- app/workflow/runs/[workflowId]/page.tsx
+++ app/workflow/runs/[workflowId]/page.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Topbar from "@/app/workflow/_components/topbar/Topbar";
+import { GetWorkflowExecutions } from "@/actions/workflows/getWorkflowExecutions";
 
-type Props = {};
-
-const ExecutionPage = (props: Props) => {
-  return <div>ExecutionPage</div>;
+type Props = {
+  params: { workflowId: string };
 };
+
+const ExecutionPage = ({ params }: Props) => {
+  return (
+    <div className="h-full w-full overflow-auto">
+      <Topbar
+        workflowId={params.workflowId}
+        hideButtons
+        title="All runs"
+        subtitle="List of all your workflow runs"
+      />
+      <ExecutionsTable workflowId={params.workflowId} />
+    </div>
+  );
+};
+
+async function ExecutionsTable({ workflowId }: { workflowId: string }) {
+  const executions = await GetWorkflowExecutions(workflowId);
+  if (!executions) {
+    return <div>No data</div>;
+  }
+}
 
 export default ExecutionPage;
