2a30c784ed62821932805fade6e02ced8f07da23
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-07-03 23:31:29 -0300

    feat: ExecutionsTableWrapper

--- app/workflow/runs/[workflowId]/page.tsx
+++ app/workflow/runs/[workflowId]/page.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Topbar from "@/app/workflow/_components/topbar/Topbar";
 import { GetWorkflowExecutions } from "@/actions/workflows/getWorkflowExecutions";
+import { InboxIcon, Loader2Icon } from "lucide-react";
 
 type Props = {
   params: { workflowId: string };
@@ -15,16 +16,44 @@ const ExecutionPage = ({ params }: Props) => {
         title="All runs"
         subtitle="List of all your workflow runs"
       />
-      <ExecutionsTable workflowId={params.workflowId} />
+      <Suspense
+        fallback={
+          <div className="flex h-full w-full items-center justify-center">
+            <Loader2Icon size={30} className="animate-spin stroke-primary" />
+          </div>
+        }
+      >
+        <ExecutionsTableWrapper workflowId={params.workflowId} />
+      </Suspense>
     </div>
   );
 };
 
-async function ExecutionsTable({ workflowId }: { workflowId: string }) {
+async function ExecutionsTableWrapper({ workflowId }: { workflowId: string }) {
   const executions = await GetWorkflowExecutions(workflowId);
   if (!executions) {
     return <div>No data</div>;
   }
+
+  if (executions.length === 0) {
+    <div className="container w-full py-6">
+      <div className="flex items-center flex-col gap-2 justify-center h-full w-full">
+        <div className="rounded-full bg-accent w-20 h-20 flex items-center justify-center">
+          <InboxIcon size={40} className="stroke-primary" />
+        </div>
+        <div className="flex flex-col gap-2 text-center">
+          <p className="font-bold">
+            No runs have been triggered yet for this workflow
+          </p>
+          <p className="text-sm text-muted-foreground">
+            You can trigger a new run in the editor page
+          </p>
+        </div>
+      </div>
+    </div>;
+  }
+
+  return <pre>{}</pre>;
 }
 
 export default ExecutionPage;
