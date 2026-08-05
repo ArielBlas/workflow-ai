cce1e5b72c6cadf998d358176d2edd1b17bacb38
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-08-04 23:51:04 -0300

    feat: DuplicateWorkflowDialog component

---
+++ app/(dashboard)/workflows/_components/DuplicateWorkflowDialog.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import React, { useCallback, useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Layers2Icon, Loader2 } from "lucide-react";
+import CustomDialogHeader from "@/components/CustomDialogHeader";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import {
+  createWorkflowSchema,
+  createWorkflowSchemaType,
+} from "@/schema/workflow";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useMutation } from "@tanstack/react-query";
+import { CreateWorkflow } from "@/actions/workflows/createWorkflow";
+import { toast } from "sonner";
+
+type Props = {
+  triggerText?: string;
+};
+
+const DuplicateWorkflowDialog = ({ triggerText }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<duplicateWorkflowSchemaType>({
+    resolver: zodResolver(duplicatecreateWorkflowSchema),
+    defaultValues: {},
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: DuplicateWorkflow,
+    onSuccess: () => {
+      toast.success("Workflow duplicated", { id: "duplicate-workflow" });
+    },
+    onError: () => {
+      toast.error("Failed to duplicate workflow", { id: "duplicate-workflow" });
+    },
+  });
+
+  const onSubmit = useCallback(
+    (values: duplicateWorkflowSchemaType) => {
+      toast.loading("Duplicating workflow...", { id: "duplicate-workflow" });
+      mutate(values);
+    },
+    [mutate],
+  );
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        form.reset();
+        setOpen(open);
+      }}
+    >
+      <DialogTrigger>
+        <Button>{triggerText ?? "Duplicate Workflow"}</Button>
+      </DialogTrigger>
+      <DialogContent className="px-0">
+        <CustomDialogHeader
+          icon={Layers2Icon}
+          title="Create Workflow"
+          subTitle="Start building your workflow"
+        />
+        <div className="p-6">
+          <Form {...form}>
+            <form
+              className="space-y-8 w-full"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex gap-1 items-center">
+                      Name
+                      <p className="text-xs text-primary">(required)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Choose a descriptive and unique name
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex gap-1 items-center">
+                      Description
+                      <p className="text-xs text-muted-foreground">
+                        (optional)
+                      </p>
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea className="resize-none" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Provide a brief description of what your workflow does.
+                      <br /> This is optinal but can help you remember the
+                      workflow&pos;s purpose
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full" disabled={isPending}>
+                {!isPending ? "Proceed" : <Loader2 className="animate-spin" />}
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DuplicateWorkflowDialog;
