b5496d65fc1734847e614dedaf52ee93f8990cf6
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-08-12 23:57:30 -0300

    feat: ClickElementTask task

--- lib/workflow/task/registry.tsx
+++ lib/workflow/task/registry.tsx
@@ -4,6 +4,7 @@ import { ExtractTextFromElementTask } from "@/lib/workflow/task/ExtractTextFromE
 import { TaskType } from "@/types/task";
 import { WorkflowTask } from "@/types/workflow";
 import { FillInputTask } from "./FillInput";
+import { ClickElementTask } from "./ClickElement";
 
 type Registry = {
   [K in TaskType]: WorkflowTask & { type: K };
@@ -14,4 +15,5 @@ export const TaskRegistry: Registry = {
   PAGE_TO_HTML: PageToHtmlTask,
   EXTRACT_TEXT_FROM_ELEMENT: ExtractTextFromElementTask,
   FILL_INPUT: FillInputTask,
+  CLICK_ELEMENT: ClickElementTask,
 };
