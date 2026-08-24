d2793b00874ad862c5cdf88e15620443ba88004b
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-08-23 23:59:32 -0300

    feat: Credentials page

---
+++ app/(dashboard)/credentials/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {};
+
+const CredentialsPage = (props: Props) => {
+  return (
+    <div className="flex flex-1 flex-col h-full">
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold">Credentials</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CredentialsPage;
