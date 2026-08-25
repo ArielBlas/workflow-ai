9a37e61d69cbd590ae555c118d4777e1567c2dc6
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-08-24 23:47:50 -0300

    feat: GetCredentialsForUser action

--- app/(dashboard)/credentials/page.tsx
+++ app/(dashboard)/credentials/page.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ShieldIcon } from "lucide-react";
+import React, { Suspense } from "react";
+import { GetCredentialsForUser } from "@/actions/credentials/getCredentialsForUser";
 
 type Props = {};
 
@@ -8,10 +12,31 @@ const CredentialsPage = (props: Props) => {
       <div className="flex justify-between">
         <div className="flex flex-col">
           <h1 className="text-3xl font-bold">Credentials</h1>
+          <p className="text-muted-foreground">Manage your credentials</p>
         </div>
+      </div>
+
+      <div className="h-full py-6 space-y-8">
+        <Alert>
+          <ShieldIcon className="w-4 h-4 stroke-primary" />
+          <AlertTitle className="text-primary">Encryption</AlertTitle>
+          <AlertDescription>
+            All information is securely encrypted, ensuring your data remains
+            safe
+          </AlertDescription>
+        </Alert>
+
+        <Suspense fallback={<Skeleton className="h-[300px] w-full" />}>
+          <UserCredentials />
+        </Suspense>
       </div>
     </div>
   );
 };
+
+async function UserCredentials() {
+  const credentials = await GetCredentialsForUser();
+  return <div>User creds</div>;
+}
 
 export default CredentialsPage;
