2f8927997caf4dfb6daaa631e7d9cf5727bc2c22
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-08-30 23:51:42 -0300

    feat: symmetricEncrypt lib

---
+++ lib/encryption.ts
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+
+const ALG = "aes_256-cbc";
+
+export const symmetricEncrypt = (data: string) => {
+  const key = process.env.ENCRYPTION_KEY;
+  if (!key) throw new Error("encryption key not found");
+
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(ALG, Buffer.from(key, "hex"), iv);
+
+  let encrypted = cipher.update(data);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
+};
