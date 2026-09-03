33f92f54c9a1ae0d80491b5966cd532345772c38
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-09-02 23:59:04 -0300

    feat: DeleteCredentialDialog component

--- app/(dashboard)/credentials/page.tsx
+++ app/(dashboard)/credentials/page.tsx
@@ -6,6 +6,7 @@ import { GetCredentialsForUser } from "@/actions/credentials/getCredentialsForUs
 import { Card } from "@/components/ui/card";
 import CreateCredentialDialog from "./_components/CreateCredentialDialog";
 import { formatDistanceToNow } from "date-fns";
+import DeleteCredentialDialog from "./_components/DeleteCredentialDialog";
 
 type Props = {};
 
@@ -72,15 +73,18 @@ async function UserCredentials() {
         });
 
         return (
-          <Card key={credential.id}>
-            <div>
-              <div>
+          <Card key={credential.id} className="w-full p-4 flex justify-between">
+            <div className="flex gap-2 items-center">
+              <div className="rounded-full bg-primary/10 w-8 h-8 flex items-center justify-center">
                 <LockKeyholeIcon size={18} className="stroke-primary" />
               </div>
               <div>
-                <p>{credential.name}</p>
+                <p className="font-bold">{credential.name}</p>
+                <p className="text-sm text-muted-foreground">{createdAt}</p>
               </div>
             </div>
+
+            <DeleteCredentialDialog name={credential.name} />
           </Card>
         );
       })}
