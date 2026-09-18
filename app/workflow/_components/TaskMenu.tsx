61d8c5c05e07f6f57cfcea791ac1d7a1828c64ec
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-09-17 23:54:45 -0300

    refactor: TaskMenu storage

--- app/workflow/_components/TaskMenu.tsx
+++ app/workflow/_components/TaskMenu.tsx
@@ -20,7 +20,13 @@ const TaskMenu = (props: Props) => {
       <Accordion
         type="multiple"
         className="w-full"
-        defaultValue={["extraction", "interactions", "timing", "results"]}
+        defaultValue={[
+          "extraction",
+          "interactions",
+          "timing",
+          "results",
+          "storage",
+        ]}
       >
         <AccordionItem value="interactions">
           <AccordionTrigger className="font-bold">
@@ -38,6 +44,14 @@ const TaskMenu = (props: Props) => {
             <TaskMenuBtn taskType={TaskType.PAGE_TO_HTML} />
             <TaskMenuBtn taskType={TaskType.EXTRACT_TEXT_FROM_ELEMENT} />
             <TaskMenuBtn taskType={TaskType.EXTRACT_DATA_WITH_AI} />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="storage">
+          <AccordionTrigger className="font-bold">
+            Data Storage
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1">
+            <TaskMenuBtn taskType={TaskType.READ_PROPERTY_FROM_JSON} />
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="timing">
